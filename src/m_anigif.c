5d22ee0a333f487bd40b2b7a1be7b3a6d9bdc475
Jaime Passos <lazymyuutsu@gmail.com>
2020-09-10 01:43:46 -0300

    Change method of color look-up table generation

--- src/m_anigif.c
+++ src/m_anigif.c
@@ -478,14 +478,14 @@ static void GIF_rgbconvert(UINT8 *linear, UINT8 *scr)
 	size_t src = 0, dest = 0;
 	size_t size = (vid.width * vid.height * 3);
 
-	InitColorLUT(gif_palette);
+	InitColorLUT(gif_framepalette, true);
 
 	while (src < size)
 	{
 		r = (UINT8)linear[src];
 		g = (UINT8)linear[src + 1];
 		b = (UINT8)linear[src + 2];
-		scr[dest] = colorlookup[r >> SHIFTCOLORBITS][g >> SHIFTCOLORBITS][b >> SHIFTCOLORBITS];
+		scr[dest] = GetColorLUTDirect(r, g, b);
 		src += (3 * scrbuf_downscaleamt);
 		dest += scrbuf_downscaleamt;
 	}
