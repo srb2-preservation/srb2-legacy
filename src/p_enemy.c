781b4e51f8f845374e6c9c3c4618ee1f2e5ed666
xdf22 <nothingjust752@gmail.com>
2026-06-12 12:10:48 +0300

    mace thing idk i fucked up the cherrypick

--- src/p_enemy.c
+++ src/p_enemy.c
@@ -4725,8 +4725,8 @@ void A_MaceRotate(void *thing)
 	radius = FixedMul(actor->info->speed * actor->reactiontime, actor->target->scale);
 
 	// Double the radius if the chain links are made up of maces.
-	if (actor->target->type == MT_AXIS && (actor->type == MT_SMALLMACE || actor->type == MT_BIGMACE))
-		radius *= 2;
+	/*if (actor->target->type == MT_AXIS && (actor->type == MT_SMALLMACE || actor->type == MT_BIGMACE))
+		radius *= 2;*/
 
 	// Axis offset for the axis.
 	radius += actor->target->extravalue1;
@@ -4780,7 +4780,6 @@ void A_MaceRotate(void *thing)
 
 		actor->threshold += actor->target->lastlook;
 		actor->threshold &= FINEMASK;
-		actor->target->health &= FINEMASK;
 
 		fa = actor->threshold;
 		v[0] = FixedMul(FINECOSINE(fa), radius);
