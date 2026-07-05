d8b0942a6fb559cd045f5cc40f5454c8940fbe7d
chromaticpipe <chromaticpipe@gmail.com>
2026-07-05 12:51:06 -0500

    Fix level interpolators on midgame join

--- src/d_netfil.c
+++ src/d_netfil.c
@@ -351,7 +351,6 @@ INT32 CL_CheckFiles(void)
 {
 	INT32 i, j;
 	char wadfilename[MAX_WADPATH];
-	size_t packetsize = 0;
 	size_t filestoload = 0;
 	boolean downloadrequired = false;
 
@@ -422,8 +421,6 @@ INT32 CL_CheckFiles(void)
 				return 4;
 			}
 		}
-
-		packetsize += nameonlylength(fileneeded[i].filename) + 22;
 		fileneeded[i].status = findfile(fileneeded[i].filename, fileneeded[i].md5sum, true);
 		CONS_Debug(DBG_NETPLAY, "found %d\n", fileneeded[i].status);
 		return 4;
