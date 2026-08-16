99b8d24452a7b7b2e1dea1abe57e68b421134bf6
chromaticpipe <chromaticpipe@gmail.com>
2026-08-16 13:27:56 -0500

    Why are we invalidating colormaps that don't exist

--- src/lua_script.c
+++ src/lua_script.c
@@ -408,6 +408,7 @@ void LUA_InvalidateLevel(void)
 {
 	thinker_t *th;
 	size_t i;
+	ffloor_t *rover = NULL;
 	if (!gL)
 		return;
 
@@ -417,13 +418,19 @@ void LUA_InvalidateLevel(void)
 	LUA_InvalidateMapthings();
 
 	for (i = 0; i < numsubsectors; i++)
-	{
 		LUA_InvalidateUserdata(&subsectors[i]);
+	for (i = 0; i < numsectors; i++)
+	{
+		LUA_InvalidateUserdata(&sectors[i]);
+		LUA_InvalidateUserdata(&sectors[i].lines);
 		if (sectors[i].extra_colormap)
 			LUA_InvalidateUserdata(sectors[i].extra_colormap);
+		if (sectors[i].ffloors)
+		{
+			for (rover = sectors[i].ffloors; rover; rover = rover->next)
+				LUA_InvalidateUserdata(rover);
+		}
 	}
-	for (i = 0; i < numsectors; i++)
-		LUA_InvalidateUserdata(&sectors[i]);
 	for (i = 0; i < numlines; i++)
 	{
 		LUA_InvalidateUserdata(&lines[i]);
