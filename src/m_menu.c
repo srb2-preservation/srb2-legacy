7113565f0f840319eaa088ce43e27bb40c6f6391
xdf22 <nothingjust752@gmail.com>
2026-08-18 18:36:29 +0300

    "that comment is outdated" -chromaticpipe Tuesday, 18 August 2026 at 18:31

--- src/m_menu.c
+++ src/m_menu.c
@@ -1666,7 +1666,7 @@ static menuitem_t OP_LegacyOptionsMenu[] =
 	{IT_STRING|IT_CALL, NULL, "Report an Issue", "If you found a problem with SRB2 Legacy or have a suggestion, here is where to go.", M_LegacyReportIssue, 90},
 };
 
-static menuitem_t OP_LegacyCreditsMenu[] = // This barely fits on green resolution
+static menuitem_t OP_LegacyCreditsMenu[] = // This barely fits on green resolutions
 {
 	{IT_HEADER|IT_STRING, NULL, "Contributors:", NULL,  NULL, 3},
 	{IT_STRING, NULL, "PAS", NULL,  NULL, 11},  
