0d2260bbd700bc1c0819844c9cdabe3445a8d7c6
PAS <pasrc@proton.me>
2026-08-20 07:48:25 -0400

    Fix i_video.c indentation

--- src/glfw/i_video.c
+++ src/glfw/i_video.c
@@ -61,7 +61,7 @@ void I_StartupGraphics(void)
 	memset(pal32, 0, sizeof(pal32));
 	CV_RegisterVar(&cv_vidwait);
 
-    	if (!glfwInit())
+	if (!glfwInit())
 		I_Error("Failed to initialize GLFW");
 
 	VID_SetMode(20);
@@ -81,20 +81,20 @@ void VID_StartupOpenGL(void){}
 
 void I_SetPalette(RGBA_t *pal)
 {
-    if (!pal)
-        return;
-
-    for (int i = 0; i < 256; i++)
-    {
+	if (!pal)
+		return;
+	
+	for (int i = 0; i < 256; i++)
+	{
 		pal32[i] =
 			(255 << 24) |
 			((UINT32)pal[i].s.blue  << 16) |
 			((UINT32)pal[i].s.green << 8)  |
 			((UINT32)pal[i].s.red);
-    }
+	}
 
-    if (vid.buffer)
-        memset(vid.buffer, 0, vid.width * vid.height);
+	if (vid.buffer)
+		memset(vid.buffer, 0, vid.width * vid.height);
 }
 
 INT32 VID_NumModes(void)
@@ -116,22 +116,22 @@ void I_SetResolution(INT32 width, INT32 height) {}
 INT32 VID_SetMode(INT32 modeNum)
 {
 	vid.recalc = 1;
-    vid.width = windowedModes[modeNum][0];;
-    vid.height = windowedModes[modeNum][1];;
-    vid.rowbytes = vid.width;
-    vid.bpp = 1;
-
-    vid.buffer = calloc(1, vid.width * vid.height);
+	vid.width = windowedModes[modeNum][0];;
+	vid.height = windowedModes[modeNum][1];;
+	vid.rowbytes = vid.width;
+	vid.bpp = 1;
+	
+	vid.buffer = calloc(1, vid.width * vid.height);
 
 	VID_CheckRenderer();
-    return 0;
+	return 0;
 }
 
 void VID_CheckRenderer(void)
 {
 	GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
-    free(rgba);
-    rgba = calloc(1, vid.width * vid.height * 4);
+	free(rgba);
+	rgba = calloc(1, vid.width * vid.height * 4);
 
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
@@ -142,12 +142,12 @@ void VID_CheckRenderer(void)
 		window = NULL;
 	}
 
-    window = glfwCreateWindow(vid.width, vid.height, "SRB2 Legacy "VERSIONSTRING, cv_fullscreen.value ? primaryMonitor : NULL, NULL);
-    if (!window)
+	window = glfwCreateWindow(vid.width, vid.height, "SRB2 Legacy "VERSIONSTRING, cv_fullscreen.value ? primaryMonitor : NULL, NULL);
+	if (!window)
 		I_Error("Failed to create window!\n");
 
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
 
 	glGenTextures(1, &screenTex);
 	glBindTexture(GL_TEXTURE_2D, screenTex);
@@ -157,8 +157,8 @@ void VID_CheckRenderer(void)
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, vid.width, vid.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 
-    glDisable(GL_DEPTH_TEST);
-    glClearColor(0,0,0,1);
+	glDisable(GL_DEPTH_TEST);
+	glClearColor(0,0,0,1);
 
 	if (cv_fullscreen.value == 1)
 	{
@@ -199,7 +199,7 @@ void I_UpdateNoBlit(void) {}
 void I_FinishUpdate(void)
 {
 	if (!window && !screens[0] && !vid.buffer)
-    	return;
+		return;
 
 	SCR_CalculateFPS();
 
@@ -237,8 +237,8 @@ void I_FinishUpdate(void)
 
 	glDisable(GL_TEXTURE_2D);
 
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+	glfwSwapBuffers(window);
+	glfwPollEvents();
 }
 
 void I_UpdateNoVsync(void) {}
@@ -251,7 +251,7 @@ void I_WaitVBL(INT32 count)
 void I_ReadScreen(UINT8 *scr)
 {
 	if (!screens[0] && !scr)
-    	return;
+		return;
 
 	VID_BlitLinearScreen(screens[0], scr, vid.width*vid.bpp, vid.height, vid.rowbytes, vid.rowbytes);
 }
